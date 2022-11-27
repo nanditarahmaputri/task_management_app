2628554bb15c861fb41668229d46c8ee9d44e719
nanditarahmaputri <nanditananditarahma285@gmail.com>
2022-11-27 22:11:24 +0700

    commit4

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-import 'package:ionicons/ionicons.dart';
+import 'package:task_management_app/app/utils/widget/header.dart';
+import 'package:task_management_app/app/utils/widget/sideBar.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -13,182 +14,26 @@ class HomeView extends GetView<HomeController> {
         backgroundColor: Colors.blue[100],
         body: Row(
           children: [
-            Expanded(
+            const Expanded(
               flex: 2,
-              child: Container(
-                color: Colors.blue[100],
-                child: SingleChildScrollView(
-                    child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Container(
-                      height: 100,
-                      alignment: Alignment.topCenter,
-                      child: const Image(
-                        image: AssetImage('assets/icon/icon.png'),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 100,
-                      child: Center(
-                          child: InkWell(
-                        child: Column(children: [
-                          Container(
-                            height: 40,
-                            width: 100,
-                            decoration: Get.currentRoute == '/home'
-                                ? BoxDecoration(
-                                    borderRadius: BorderRadius.circular(40),
-                                    color: Colors.white,
-                                  )
-                                : BoxDecoration(),
-                            child: const Icon(
-                              Ionicons.desktop,
-                              color: Colors.grey,
-                              size: 30,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text(
-                            'Home',
-                            style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 16,
-                            ),
-                          ),
-                        ]),
-                      )),
-                    ),
-                    SizedBox(
-                      height: 100,
-                      child: Center(
-                          child: InkWell(
-                        child: Column(children: [
-                          Container(
-                            height: 40,
-                            width: 100,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(40),
-                                color: Colors.white),
-                            child: const Icon(
-                              Ionicons.desktop,
-                              color: Colors.grey,
-                              size: 30,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text(
-                            'Home',
-                            style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 16,
-                            ),
-                          ),
-                        ]),
-                      )),
-                    ),
-                    SizedBox(
-                      height: 100,
-                      child: Center(
-                          child: InkWell(
-                        child: Column(children: [
-                          Container(
-                            height: 40,
-                            width: 100,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(40),
-                                color: Colors.white),
-                            child: const Icon(
-                              Ionicons.desktop,
-                              color: Colors.grey,
-                              size: 30,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text(
-                            'Home',
-                            style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 16,
-                            ),
-                          ),
-                        ]),
-                      )),
-                    ),
-                    SizedBox(
-                      height: 100,
-                      child: Center(
-                          child: InkWell(
-                        child: Column(children: [
-                          Container(
-                            height: 40,
-                            width: 100,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(40),
-                                color: Colors.white),
-                            child: const Icon(
-                              Ionicons.desktop,
-                              color: Colors.grey,
-                              size: 30,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text(
-                            'Home',
-                            style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 16,
-                            ),
-                          ),
-                        ]),
-                      )),
-                    ),
-                    SizedBox(
-                      height: 100,
-                      child: Center(
-                          child: InkWell(
-                        child: Column(children: [
-                          Container(
-                            height: 40,
-                            width: 100,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(40),
-                                color: Colors.white),
-                            child: const Icon(
-                              Ionicons.desktop,
-                              color: Colors.grey,
-                              size: 30,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text(
-                            'Home',
-                            style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 16,
-                            ),
-                          ),
-                        ]),
-                      )),
-                    ),
-                  ],
-                )),
-              ),
+              child: SideBar(),
             ),
             Expanded(
               flex: 15,
-              child: Container(
-                color: Colors.white,
+              child: Column(
+                children: [
+                  const header(),
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.all(50),
+                      margin: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             )
           ],
